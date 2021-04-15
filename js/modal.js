46fe71c0a169f78e125f4bc0c5c7e0273a48137e
RobsonNascimentoDev <77898762+RobsonNascimentoDev@users.noreply.github.com>
2021-04-15 20:44:20 -0300

    trabalho Final
    Criação do botão de inicio

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,22 @@
 const iniciaModal = document.querySelectorAll('[data-modal-target]')
 const botaoFechar = document.querySelectorAll('[data-close-button]')
+const botaoIniciar = document.querySelectorAll('[data-start-button]')
 
 iniciaModal.forEach(e => {
     const modal = document.querySelector(e.dataset.modalTarget)
     abreModal(modal)
 })
 
-
 botaoFechar.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal')
+        fechaModal(modal)
+    })
+})
+
+botaoIniciar.forEach(b =>{
+    b.addEventListener('click', () =>{
+        const modal = b.closest('.modal')
         fechaModal(modal)
     })
 })
